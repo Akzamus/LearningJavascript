329345f78a2f3812eb6bf5c4848342fbe6686b48
Akzam Khamitov <makoron3002@gmail.com>
2022-09-09 16:23:23 +0600

    first commit

---
+++ firstStart/js/script.js
@@ -0,0 +1,40 @@
+"use strict";
+
+// let number = 5;
+// console.log("number = " + number);
+// const LEFT_BORDER_WITH = 1;
+//
+// console.log("LEFT_BORDER_WITH = " + LEFT_BORDER_WITH);
+//
+// number = 10;
+// console.log("number = " + number);
+//
+// const obj = {
+//     a: 50
+// };
+//
+// obj.a = 10;
+// console.log(obj);
+
+
+// alert("Hello world");
+
+// let result = confirm("Yes or no?");
+// console.log(result);
+
+// const answer = prompt("Вам есть 18?", "18");
+// console.log(answer);
+
+// const answers = [];
+// answers[0] = prompt("Firstname:", "");
+// answers[1] = prompt("Lastname:", "");
+// answers[2] = +prompt("Age:", "");
+// document.write(answers);
+
+// const answers = [];
+// console.log(typeof(answers));
+// console.log(typeof(null));
+
+// const category = "toys";
+// console.log("https://someurl.com/" + category);
+// console.log(`https://someurl.com/${category}/5`);
