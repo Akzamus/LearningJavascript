6e411408a1833fce6fb28a2c2276c379ce6d6089
Akzam Khamitov <makoron3002@gmail.com>
2022-09-11 03:23:07 +0600

    did tasks

---
+++ HWs/6.js
@@ -0,0 +1,13 @@
+const lines = 5;
+let result = '';
+
+for(let i = 1, j = 0; i <= 11; i = i + 2, j++) {
+    for (let k = j; k < lines; k++) {
+        result+=" ";
+    }
+    for(let k = 0; k < i; k++) {
+        result+="*";
+    }
+    result+="\n";
+}
+console.log(result);
